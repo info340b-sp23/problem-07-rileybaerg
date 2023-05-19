1010c1d6c55801fd8df09c2aace35744987f52bf
RIBBLY <rbaerg@uw.edu>
2023-05-19 13:42:25 -0700

    problem b passes all of its tests 7/7, problem was with nested components

--- problem-b/src/components/PetList.js
+++ problem-b/src/components/PetList.js
@@ -1,28 +1,30 @@
 import React from 'react';
 
-function PetList({ pets, onAdopt }) {
-  function PetCard({ petData }) {
-    const handleClick = () => {
-      onAdopt(petData.name);
-    };
+function PetCard({ petData, onAdopt }) {
+  const handleClick = () => {
+    onAdopt(petData.name);
+  };
 
-    return (
-      <div className='card' onClick={handleClick}>
-        <img className='card-img-top' src={petData.img} alt={petData.name} />
-        <div className='card-body'>
-          <h3 className='card-title'>{petData.adopted ? petData.name + ' (Adopted)': petData.name}</h3>
-          <p className='card-text'>{petData.sex} {petData.breed}</p>
-        </div>
+  return (
+    <div className='card' onClick={handleClick}>
+      <img className='card-img-top' src={petData.img} alt={petData.name} />
+      <div className='card-body'>
+        <h3 className='card-title'>{petData.adopted ? petData.name + ' (Adopted)': petData.name}</h3>
+        <p className='card-text'>{petData.sex} {petData.breed}</p>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
+
+
+function PetList({ pets, onAdopt }) {
   return (
     <>
       <h2>Dogs for Adoption</h2>
       <div className='card-deck'>
         {pets.map((pet) => (
-          <PetCard key={pet.id} petData={pet} />
+          <PetCard key={pet.id} petData={pet} onAdopt={onAdopt} />
         ))}
       </div>
     </>
